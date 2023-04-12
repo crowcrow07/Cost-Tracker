eb2fc6dfbb46b21187b498908ff9efa3f59e8d8c
crowcrow07 <skathd3402@gmail.com>
2023-04-12 20:32:40 +0900

    state추가

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,22 @@
+import { useState } from "react";
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem({ title, amount, date }) {
+  const [changeTitle, setChangeTitle] = useState(title);
+  const clickHandler = () => {
+    setChangeTitle("update");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{changeTitle}</h2>
         <div className="expense-item__price">{amount}만원</div>
       </div>
-      <button>Change Title</button>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
